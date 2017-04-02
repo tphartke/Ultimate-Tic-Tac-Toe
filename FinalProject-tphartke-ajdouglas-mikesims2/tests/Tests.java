f747d5c3c88e7af46ebe5538cdc80f869993f91e
Timmy Hartke <tphartke@bsu.edu>
2017-04-02 12:34:39 -0400

    Added Tests and Fixed Player TextField errors

--- FinalProject-tphartke-ajdouglas-mikesims2/tests/Tests.java
+++ FinalProject-tphartke-ajdouglas-mikesims2/tests/Tests.java
@@ -6,33 +6,12 @@ public class Tests {
     private myActionListener listener;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         Turn.turnNumber = 1;
         listener = new myActionListener();
         @SuppressWarnings("Variable 'ui' is never used")
         UI ui = new UI(); //a UI must be initialized in order for the tests to run properly
         UI.newGameButton.doClick();
-    }
-
-    @Test
-    public void testPlayerOneScoreboard() {
-        UI.boardButtons[2][0][2].doClick();
-        UI.boardButtons[6][2][0].doClick();
-        UI.boardButtons[1][0][1].doClick();
-        UI.boardButtons[3][1][0].doClick();
-        UI.boardButtons[0][0][0].doClick();
-        Assert.assertEquals(UI.player1Score.getText(), "6");
-    }
-
-    @Test
-    public void testPlayerTwoScoreboard() {
-        UI.boardButtons[3][2][0].doClick();
-        UI.boardButtons[2][0][2].doClick();
-        UI.boardButtons[6][2][0].doClick();
-        UI.boardButtons[1][0][1].doClick();
-        UI.boardButtons[3][1][0].doClick();
-        UI.boardButtons[0][0][0].doClick();
-        Assert.assertEquals(UI.player1Score.getText(), "3");
     }
     @Test
     public void testWriteXToBoardLocation(){
