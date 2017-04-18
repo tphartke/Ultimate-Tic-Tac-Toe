19117e92d250e5e0059c68ec11367dfb8bd569ff
Ashley Douglas <ajdouglas@bsu.edu>
2017-04-18 15:36:11 -0400

    The leaderboard can now read from the text file
    Signed-off-by: ajdouglas <ajdouglas@bsu.edu>

---
+++ FinalProject-tphartke-ajdouglas-mikesims2/src/LeaderboardReader.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class LeaderboardReader {
+
+    private static final String FILENAME = "src/Files/leaderboardStorage.txt";
+
+    static String scoreboardReader() throws IOException {
+
+        BufferedReader bufferedReader;
+
+        String CurrentLine;
+        bufferedReader = new BufferedReader(new FileReader(FILENAME));
+
+        while ((CurrentLine = bufferedReader.readLine()) != null) {
+            SecondPanel.scoreboardData.append(CurrentLine);
+        }
+        return "";
+    }
+}
+
--- FinalProject-tphartke-ajdouglas-mikesims2/src/SecondPanel.java
+++ FinalProject-tphartke-ajdouglas-mikesims2/src/SecondPanel.java
@@ -1,22 +1,46 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
-class SecondPanel extends JFrame{
-    private JTextArea playerScores;
-    private JPanel panel1;
+
+class SecondPanel extends JFrame {
 
     private JFrame frame = new JFrame("Tic-Tac-Toe LeaderBoard");
     private static final int FRAME_HEIGHT = 700;
     private static final int FRAME_WIDTH = 1200;
+    private JPanel panel = new JPanel(new BorderLayout());
+    static JTextArea scoreboardData = new JTextArea();
 
-    SecondPanel(){
+
+    SecondPanel() {
         super();
         createFrame();
+        initialize();
         frame.setVisible(true);
     }
 
-    private void createFrame(){
+    private void createFrame() {
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    private void initialize() {
+        addComponentsToPanel();
+        frame.add(panel);
+    }
+
+    private void addComponentsToPanel() {
+        addScoreboard();
+        panel.add(scoreboardData);
+        scoreboardData.setEditable(false);
+    }
+
+    private void addScoreboard() {
+        try {
+            scoreboardData.append(LeaderboardReader.scoreboardReader());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
